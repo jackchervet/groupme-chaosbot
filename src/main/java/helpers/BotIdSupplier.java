e36d1f8381e53f1ca3a547ba6dba17b0dc126101
jackchervet <jachervet@ucdavis.edu>
2020-02-29 17:38:45 -0500

    BotIdSupplier framework

--- src/main/java/helpers/BotIdSupplier.java
+++ src/main/java/helpers/BotIdSupplier.java
@@ -1,4 +1,11 @@
 package helpers;
 
+import java.util.function.Supplier;
+
 public class BotIdSupplier {
+    private static final Supplier<String> BOT_ID_SUPPLIER = () -> "";
+
+    public static Supplier<String> get() {
+        return BOT_ID_SUPPLIER;
+    }
 }
